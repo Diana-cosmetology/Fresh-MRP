89ef0f1bad36a48306340068645bd155887f7ae1
deksden <deksden@deksden.com>
2018-05-15 01:03:59 +0700

    - (enh) more server visualization for products/product data

--- server/controllers/erp-controller.js
+++ server/controllers/erp-controller.js
@@ -1,11 +1,18 @@
+/* eslint-disable arrow-body-style */
+import _ from 'lodash'
 
 export default module.exports = (app) => {
   return {
     getProducts: (req, res) => {
-      const products = app.data.products
+      const { products } = app.data
 
-      res.render('products', { "products": products })
+      res.render('products', { products })
       return {}
-    }
+    },
+    getProduct: (req, res) => {
+      const { productId } = req.params
+
+      res.render('product', { productId, product: _.find(app.data.products, { productId }) })
+    },
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
 
  Simple server for ERP project: static content + some JSON visualization
@@ -7,25 +8,27 @@
 // import packages
 import path from 'path'
 import express from 'express'
+import bodyParser from 'body-parser'
 
 // import project modules
 import ErpController from './controllers/erp-controller'
-import products from '../data/products'
+import products from '../data/products.json'
 
 const app = express()
 
 // configure express:
 const staticPath = path.join(__dirname, 'public')
 app.use(express.static(staticPath))
-app.set("view engine", "pug")
-app.set("views", path.join(__dirname, "views"))
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+app.use(bodyParser.json())
 
 // unility function for async controllers for routes:
 // eslint-disable-next-line no-param-reassign
 app.wrap = fn => (req, res, next) => {
   try {
     const ret = fn(req, res)
-    if (ret.then) {
+    if (ret && ret.then) {
       ret.then(() => next())
         .catch((err) => { next(err) })
     } else {
@@ -45,11 +48,12 @@ app.data.products = products
 
 // configure routes:
 app.get('/erp/products', app.wrap(erpController.getProducts))
+app.get('/erp/products/:productId', app.wrap(erpController.getProduct))
 
 // start server:
-var server = app.listen(3000, function () {
-  var host = 'localhost';
-  var port = server.address().port;
-  console.log('listening on http://'+host+':'+port+'/');
-});
+const server = app.listen(3000, () => {
+  const host = 'localhost'
+  const { port } = server.address()
+  console.log(`listening on http://${host}:${port}/`)
+})
 
